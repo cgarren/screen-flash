4900e003eef240c877dea702c9af3d8f5c9651bf
Cooper Garren <cgarren18@icloud.com>
2022-11-05 14:30:24 -0400

    new type of animation function

--- animate.js
+++ animate.js
@@ -1,26 +1,45 @@
-var index = 0;
-var startTime;
-var colors = ['red', 'white'];
-var id = null;
+let index = 0;
+let startTime;
+let id = null;
+let prevPerfTime = 0;
 
+const colors = ['red', 'white'];
 const hertz = 1;
 const interval = 1 / hertz * 1000 / colors.length;
+
+function animate2() {
+  setInterval(() => {
+    if (index == 0) {
+      const now = performance.now();
+      const delta = now - prevPerfTime;
+      prevPerfTime = now;
+      console.log(delta);
+    }
+    document.getElementById("mydiv").style.backgroundColor = colors[index];
+    if (++index > colors.length - 1) { index = 0; }
+  }, interval);
+}
 
 function animate(time) {
   id = requestAnimationFrame(animate);
 
   if (!startTime) { startTime = time; }
-  var elapsed = time - startTime;
-  if (elapsed > interval) {
-		startTime = time;
-		console.log("setting color to " + colors[index]);
-		document.getElementById("mydiv").style.backgroundColor = colors[index];
+  if (time - startTime > interval) {
+    startTime = time;
+    document.getElementById("mydiv").style.backgroundColor = colors[index];
+    if (index == 0) {
+      currentPerfTime = performance.now();
+      console.log(currentPerfTime - prevPerfTime);
+      prevPerfTime = currentPerfTime;
+    }
 		if (++index > colors.length - 1) { index = 0; }
   }
 }
 
 function startBlink() {
+  document.getElementById("mydiv").style.backgroundColor = colors[-1];
   requestAnimationFrame(animate);
+  //animate2();
 }
 
 startBlink();
